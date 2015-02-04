b3239f5f69579c564bb0c1a1a29d4dac65d9b49b
Meysam Pour Ganji <p.g.meysam@zentexco.com>
2015-02-04 11:07:27 +0330

    Initial commit for CSV2SQL Covertor

---
+++ csv2sql/csv2sql.php
@@ -0,0 +1,55 @@
+<?php
+if($argc < 3)
+{
+	echo "Use in correct format:\n";
+	echo "\tphp csv2sql.php inputFileName.csv outputFileName.sql [verbose (0/1)]\n";
+	exit();
+}
+elseif( !file_exists($argv[1]) || !is_file($argv[1]) )
+{
+	echo "File '" . $argv[1] . "' does not exists or is a folder name.\n";
+	exit();
+}
+elseif( !is_readable($argv[1]) )
+{
+	echo "I can't read '" . $argv[1] . "', please check permission of file.\n";
+	exit();
+}
+elseif( ($outFP = fopen($argv[2], 'w+')) === false )
+{
+	echo "I can't write to file with '" . $argv[2] . "' name.\n";
+	exit();
+}
+
+if( ($fp = fopen($argv[1], 'r')) !== false )
+{
+	$id = 1;
+	$lang = 'fa';
+	$buffer = '';
+	
+	$headOfSqlCommand = 'INSERT INTO `messagetranslation` (`id`, `translation`, `language`, `messagesource_id`) VALUES ';
+	
+	while( ( ($line = fgetcsv($fp)) !== false ) && !feof($fp)  )
+	{
+		$buffer .= $headOfSqlCommand . "('" . $id . "', '" . $line[3] . "', '" . $lang . "', '" . $line[0] . "');\n";
+		
+		++$id;
+		
+		if($id % 100 === 0)
+		{
+			fwrite($outFP, $buffer);
+			$buffer = '';
+			
+			if( isset($argv[3]) && (int)$argv[3] )
+			{
+				echo "Buffer flushed.\n";
+			}
+		}
+	}
+	
+	fclose($fp);
+	fclose($outFP);
+	
+	echo "Mission Completed! :)\n";
+}
+?>
